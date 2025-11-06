4f97ebeb7003f57a853211962d305aab17bf15bc
vikas singh <vikas.singh66551@gmail.com>
2025-11-06 20:44:27 +0530

    Start  the  JS SEC Part

---
+++ JS FULL/Operator.js
@@ -0,0 +1,72 @@
+
+
+// 1} Comparison Operators
+//  == , === , != , !== , > , < , >=,  <=
+
+// ==   not strict compare never use This
+// ===  strict compare ALWAYS use this👍
+
+//  !=   never use  this eg :- 12 != '12' -->ans flase but it s true
+//  !==  always use  this  eg:- 12 !=='12' --> ans is true
+
+// -----------------------------------------------------------------------------------------
+
+// 2} logical Operators
+//  && ,  || , !=
+
+// && --> T && T =T
+// || --> f||f = f only ,--> not t||f = f
+// != --> T = F and F =T
+
+//  eg  username === exist && password === userpassword.
+
+
+// ---------------------------------------------------------------
+
+// 3} Teranary Operator
+//  ?  : ---> it is lke if else
+//  condtions ? dothis : dothan
+
+//   12>13 ? console.log("not is greater no ") : console.log("it is greater");
+
+// ----------------------------------------------------------------------------
+
+// 4} Type Checking Operators
+// typeof
+// instance of
+// kuchh bhi banaaoge wo top pe likhi gayi cheejo se aata hai
+
+// ----------------------------------------------------------------------
+
+// 5} string operators
+//  + --> are you to add the string (+ for concatrnations)
+
+//   "vikas"+"singh"
+
+// -----------------------------------------------------------------------------------
+
+// 6} Spread --> array nd object main  / Read Operator  --> functions main
+
+// ...(very important in modern js )
+//   let arr=[1,2,3,4];
+//   let  arr1= [...arr];   ---> o/p -> 1234
+
+// rest --> for functions
+// function abc(a,b,c,...rest){}
+// abc(1,2,3,4,5)
+
+// -----------------------------------------------------------------------------------
+
+// 7}  Nullish Coalescing Operator
+//  ?? (fallback only when null / undefined)
+
+//  user ?
+
+// ---------------------------------------------------------------
+
+
+// Optional Chaining
+//  ?.(safe access to nested values)
+
+// -----------------------------------------------------------
+
---
+++ JS FULL/loops.js
@@ -0,0 +1,115 @@
+// pure  beginner  practric
+// 1 . print  no from 1 to 10 loop from 11 to 10 print ech no
+
+// for (let i = 0; i < 10; i++)   console.log(i)
+
+// ----------------------------------------------------
+
+//  2 print only even no from 1 to 20 using loop nd condition to print only even
+
+//  for(let i=0; i<20; i++)
+//     if(i% 2 === 0)
+//         console.log("it is even no  : " + i);
+//     else
+//         console.log("it is old no :" +i)
+
+// ----------------------------------------------------
+
+// 3 print the no from 10 to 1 reverse loop with a dec
+// for(let i=10; i>0; i--)  console.log(i);
+
+// -------------------------------------------------------
+
+// 4  print the " yes " 5 times Repeat using loop
+
+//  for(let i=0; i<5;i++) console.log("yes")
+
+// --------------------------------------------------
+
+// 5 print  whether no from 1 to 10 are even or odd
+
+// for (let i = 0; i < 10; i++)
+//     if (i % 2 == 0) console.log("even =" + i);
+//     else console.log(`" odd" - ${i}`);
+
+//---------------------------------------------------------------------------
+
+//   6 Ask user for a no and say if it is  postivie or negative
+
+//  prompt --> jab bhi prompt mangoge to socho kya wo no hai aur agar hai to convert karna padega
+//   type  --> 1} parseInt 2} number 3} +prompt
+
+//  let num = +prompt("enter the no");
+//  if(num>0) console.log("it is positive");
+//  else console.log(" it is negtive");
+
+// -----------------------------------------------------------------------
+
+// 7. Ask user’s age and check if eligible to vote
+// If age >= 18 → “Eligible”, else → “Not eligible”
+
+// let age=96;
+//  or
+
+// let age =+ prompt( " enter the age : ")
+// if(age >= 18) console.log("Eligible")
+// else console.log("Not Eligible")
+
+
+// ---------------------------------------------------------
+
+// 8. Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("5 × " + i + " = " + (5 * i));
+// }
+
+// for (let i = 1; i <= 10; i++)
+//   console.log(`5 × ${i} = ${5 * i}`);
+
+
+// ------------------------------------------------------------
+// 9. Count how many numbers between 1 and 15 are greater than 8 Loop and count conditionally.
+
+// let count = 0
+// for (let i = 0; i <= 15; i++) {
+//     if (i > 8) {
+//         count++;
+//      console.log(i);
+//     }
+// }
+// console.log(` total count is ${count} `);
+
+// ------------------------------------------------------------------------
+
+
+// 10. Ask user for password and print access status
+// Hardcoded correct password. Compare with user input.
+
+// let password = "vikasbhai";
+
+// let pass = prompt(" Enter The Password ");
+
+// if (pass === null) {
+//     console.error(" You Cancelled it ");
+// } if (pass === password) {
+//     console.log("password is matched ");
+// }
+// else {
+//     console.log(" Password not matched");
+// }
+
+// ===============================================================================
+
+// Level 2 – Slightly Tougher but Logical
+
+// 11. Allow only 3 attempts to enter correct password
+// If user gets it right early, stop. If not → “Account locked”
+
+
+  let pass = prompt("Enter The Password ");
+
+  while ( ) {
+    
+  }
