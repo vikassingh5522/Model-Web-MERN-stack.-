2f2a9eacc4b8301be75e2daa0cd496aeb680f252
vikas singh <vikas.singh66551@gmail.com>
2025-11-11 17:25:45 +0530

    Start  the  JS function done 1 part

--- JS FULL/function.js
+++ JS FULL/function.js
@@ -176,7 +176,7 @@
 // abc(12);
 
 // Impure Function  ---> ek aiisa function --> sme i/p  = diff diff o/p that is call IMpure function
-  
+
 //  let  a =12;
 // function abc(val) {
 //     console.log(Math.random() + val);
@@ -184,19 +184,19 @@
 // abc(12);
 // abc(12);
 
- 
+
 
 // ------------------------------------------------------------------------------------
 
 //  - Understanding Scoping in JS - ['Global scope 'Function scope']
-   
+
 
 
 
 // -----------------------------------------------------------------------------------
 // 11 - Understanding 'Closures', 'Scoping Rule
 //   Closures --> ek function hi jo return karta hai functions  but retuning functiion jo ka  kai variable use karega
- 
+
 //  function abc() {
 //     let a=12;
 //     return function () {
@@ -204,3 +204,141 @@
 //     }
 //  }
 //  abc();
+
+
+// -------------------------------------------------------------------------------------------
+
+//   Questions 🔍🔥👍
+
+// 1} Write the function sayhello() that prints " java js"
+
+// function sayhello() {
+//     console.log("Say hello");
+// }
+// sayhello();
+
+// 2}Crreate the a functon add(a,b) that retuns their sum and log the result
+
+// function add(a,b){
+//     return a+b;
+// }
+// let ans = add(11,22);
+// console.log(ans);
+
+// 3} Write a functions with a default parameter name = " Guest" that print "Hi <name>"
+
+// function create(Guest =  " guest") {
+//     console.log(`Hii ${Guest}`);
+// }
+// create("vikas");
+
+// 4} use rest parameters to make the functions that adds unlimted number .
+
+// function addUnlimited(...num) {
+//   console.log(...num);
+// }
+// addUnlimited(1,2,3,4,6,7,8,9)
+
+// or
+
+// function addUnlimted(...num) {
+//     num.forEach(function (val) {
+//         console.log(val);
+//     });
+// }
+// addUnlimted(1,2,3,4,6,7,8,9,10);
+
+// or
+
+// function addUnlimited(...nums){
+//    let ans = nums.reduce(function (acc,  val) {
+//        return acc+val;   
+//     } ,0);
+//     console.log(ans);
+// }
+// addUnlimited(1,2,3,4,6,7,8,9,10);
+
+// 4} create  an IIIFE  that print " I am instantly"
+
+// (function create() {
+//    console.log("I am instantly ! ")
+// })();
+
+// 5} Make the a nested functions where  the inner one  print  variable from outer one 
+
+// let a = 12;
+// function prent() {
+//     let b = 11;
+//     function child() {
+//         console.log(b);
+//     }
+//     child();
+//    console.log(a);
+   
+// }
+// prent();
+
+// orr 
+
+// function prent() {
+//     let b = 11;
+//     function child() {
+//         console.log(b);
+//     }
+//     child();
+// }
+// prent();
+
+// 6} Create the  an array of 5 fruits . Add the one at the end and remove the from the beginning .
+
+//  let arr = ["aappl", " guave" , "grapes" , " mongo" , "banana"];
+//  arr.push("pear");
+//  arr.unshift("orange");
+
+// ans
+//  [
+//     "orange",
+//     "aappl",
+//     " guave",
+//     "grapes",
+//     " mongo",
+//     "banana",
+//     "pear"
+// ]
+
+// 7}  use a for loop to print all the elements of an array
+//  let arr =[1,2,3,4,5,6];
+
+//  for (let i = 0; i < array.length; i++) {
+//     console.log(arr);
+//  }
+
+// or
+
+// arr.forEach(function(elements){
+//     console.log(elements);
+// })
+
+// 8) Createa an object peron with keys name , age and city and print ech key value.
+ 
+//   let obj ={
+//     name : " vikas",
+//     age : 12,
+//     city : "jalna"
+//   };
+
+//   for(let key in obj ){
+//     console.log(obj [key]);
+//   }
+  
+  
+// 9) use setTimeout() to log " Time up" after 2 sec
+ 
+ 
+// setTimeout(function () {
+//     console.log("time up");
+// }, 9000);
+
+setTimeout(() => {
+    console.log("time up");
+}, 9000);
