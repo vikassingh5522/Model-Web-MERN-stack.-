09fcf8d46d9ffeac19300cf63bdce5ec0b5f30a5
vikas singh <vikas.singh66551@gmail.com>
2025-11-29 23:29:34 +0530

    dom 7 8 animation

--- Dom/Dom Pro   Spotlight effect. 8/project   MATRIX - Text/script.js
+++ Dom/Dom Pro   Spotlight effect. 8/project   MATRIX - Text/script.js
@@ -5,25 +5,32 @@ const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiijklmnopqrstuvwxyz";
 const text = para.innerHTML;
 
 
-para.addEventListener("mouseenter", ()=>{
-
-      setInterval(() => {
-        
-        const str = text
-            .split('')
-            .map((char,index)=>{
-            return chars.split("")[Math.floor(Math.random()*53)]
-        }).join("")
-
-        para.innerHTML =str;
-        // console.log(str)
-
-        
-      }, 100);
+// para.addEventListener("mouseenter", ()=>{
+//       setInterval(() => {   
+//         const str = text
+//             .split('')
+//             .map((char,index)=>{
+//             return chars.split("")[Math.floor(Math.random()*53)]
+//         }).join("")
+//         para.innerHTML =str;
+//         // console.log(str)
+//       }, 100);
+// });
 
 
+let iteration =0;
 
+function randomText(){
+  const str = text.split("").map((chars,index)=>{
+    if (index < iteration) {
+       return chars
+    }
+    return chars.split("")[Math.floor(Math.random()*52)]
+  }) .join("")
 
+  para.innerHTML = str;
 
+  iteration += 0.3;
 
-});
+}
+setInterval(randomText,30);
