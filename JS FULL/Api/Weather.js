9bfef1dd7a2bd0c206d6167869f47522682040a3
vikassingh5522 <vikas.singh66551@gmail.com>
2025-12-21 23:17:06 +0530

    add Weather API functionality

---
+++ JS FULL/Api/Weather.js
@@ -0,0 +1,32 @@
+
+
+// let whetherApi = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+
+
+// let whetherApi = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=8109c66605e33fe0ace3022934effd5c`;
+
+
+//after removing  lat and lon
+// let whetherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8109c66605e33fe0ace3022934effd5c`;
+
+
+async function getWeatherApi(city) {
+    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8109c66605e33fe0ace3022934effd5c`;
+
+    try {
+        const response = await fetch(weatherApi);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
+}
+
+
+
+
+
+
+export default whetherApi;
